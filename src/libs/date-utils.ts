7e5447a861e445b8fb9ed56a92b38f88d6287961
Antti Leppä <antti.leppa@metatavu.fi>
2022-09-23 07:35:42 +0300

    Added method for parsing hel.f news dates

--- src/libs/date-utils.ts
+++ src/libs/date-utils.ts
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 /**
  * Parses date
  * 
@@ -11,3 +13,17 @@ export const parseDate = (date?: string): Date | undefined => {
 
   return new Date(Date.parse(date));
 }
+
+/**
+ * Parses date object from hel.fi news format
+ * 
+ * @param dateString date string
+ * @returns parsed date
+ */
+ export const parseHelFiNewsDate = (dateString: string) => {
+  const format = "LLL d, yyyy, h:mm:s a"; 
+  const offsetIndex = dateString.lastIndexOf(" ");
+  const datePart = dateString.substring(0, offsetIndex);
+  const zonePart = dateString.substring(offsetIndex + 1).replace("GMT", "UTC");
+  return DateTime.fromFormat(datePart, format).setZone(zonePart);
+};
