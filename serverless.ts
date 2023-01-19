3cab01da08d1a2d97170cae3361c1cd0597427a6
Josh Dallos <78350882+Jdallos@users.noreply.github.com>
2023-01-19 14:59:24 +0200

    Update serverless.ts
    Co-authored-by: Ville Juutila <ville.juutila@gmail.com>

--- serverless.ts
+++ serverless.ts
@@ -144,7 +144,7 @@ const serverlessConfiguration: AWS = {
           },
         },
       },
-      Authentication: {
+      AuthenticationSessions: {
         Type: "AWS::DynamoDB::Table",
         DeletionPolicy: "Delete",
         Properties: {
