01e4e4b78c7d57fdf9dfb5582d153d194cf560b2
villejuutila <ville.juutila@gmail.com>
2023-01-20 09:49:23 +0200

    Added trigger for new lambda

--- src/functions/create-document-from-external-service/index.ts
+++ src/functions/create-document-from-external-service/index.ts
@@ -2,7 +2,16 @@ import { handlerPath } from "@libs/handler-resolver";
 import { AWSFunction } from "src/types";
 
 const fn: AWSFunction = {
-  handler: `${handlerPath(__dirname)}/handler.main`
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      schedule: {
+        rate: [
+          "cron(0 3 * * SUN *)"
+        ]
+      }
+    }
+  ]
 };
 
 export default fn;
