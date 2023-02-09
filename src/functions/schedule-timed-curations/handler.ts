7f2650eef9b9b49810a0202f87ad87e97a94edb7
Josh Dallos <78350882+Jdallos@users.noreply.github.com>
2023-02-09 08:52:49 +0200

    Update src/functions/schedule-timed-curations/handler.ts
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/functions/schedule-timed-curations/handler.ts
+++ src/functions/schedule-timed-curations/handler.ts
@@ -22,7 +22,6 @@ const scheduleTimedCuration = async () => {
   await Promise.allSettled(timedCurations.map(async timedCuration => {
     const { id, elasticCurationId, startTime, endTime, hidden, promoted, queries, curationType, documentId } = timedCuration;
 
-    const now = new Date();
     const farFuture = (DateTime.local().plus({ years: 100 })).toJSDate();
 
     const start = startTime ? parseDate(startTime) : now;
