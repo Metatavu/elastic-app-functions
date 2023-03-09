c9a09f045a92ac91e048aa1f671bd44b5881101d
Ville Juutila <ville.juutila@gmail.com>
2023-03-09 10:07:05 +0200

    Fixed conflicts

--- src/functions/add-category-to-document/handler.ts
+++ src/functions/add-category-to-document/handler.ts
@@ -88,7 +88,6 @@ const addCategoryToDocuments = async () => {
       all: [
         { url_host: "www.hel.fi" },
         { all: { url_path_dir2: ["uutiset", "news", "nyheter"] } },
-        { all: { url_path_dir2: ["uutiset", "news", "nyheter"] } },
         { none: { "meta_content_category": Object.values(ContentCategory) } }
       ]
     }
